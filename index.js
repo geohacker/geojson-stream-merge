39856fae8f54ca8749b304adfbd983190c6ba9e8
arunasank <aruna@mapbox.com>
2016-05-09 10:58:58 +0530

    Split mergeStream into its own function

--- index.js
+++ index.js
@@ -4,42 +4,45 @@ var fs = require('fs');
 var split = require('split');
 var argv = require('minimist')(process.argv.slice(2));
 
-module.exports = function () {
-    if (!argv.hasOwnProperty('file')) {
-        console.log('Usage: node index.js --file <path to line delimeted GeoJSON FeatureCollections>');
-    } else {
-        var inputFile = argv.file;
-        var outputFile = argv.output || argv.file.split('.')[0] + '-merged.json';
-        var inputStream = fs.createReadStream(inputFile, {encoding: 'utf8'}).pipe(split());
+if (!argv.hasOwnProperty('file')) {
+    console.log('Usage: node index.js --file <path to line delimited GeoJSON FeatureCollections>');
+} else {
+    mergeStream(argv.file, argv.output);
+}
 
-        var featureCollection = {
-            "type": "FeatureCollection",
-            "features": []
-        };
-
-        var start = '{"type": "FeatureCollection", "features": [';
-        fs.appendFileSync(outputFile, start, {encoding: 'utf8'});
-        var comma = "";
-        var line = 0;
-        inputStream.on('data', function (chunk) {
-            line = line + 1;
-            process.stderr.cursorTo(0);
-            process.stderr.write('Processing line: '+ String(line));
-            if (chunk) {
-                var features = JSON.parse(chunk).features;
-                features.forEach(function (feature) {
-                    fs.appendFileSync(outputFile, comma + JSON.stringify(feature), {encoding: 'utf8'});
-                    if (!comma) {
-                        comma = ',';
-                    }
-                });
-            }
-        });
-
-        inputStream.on('end', function() {
-            var end = ']}';
-            fs.appendFileSync(outputFile, end, {encoding: 'utf8'});
-            console.log('\ndone');
-        });
+function mergeStream(inputFile, outputFile) {
+    if (!outputFile) {
+        outputFile = argv.file.split('.')[0] + '-merged.json';
     }
+    var inputStream = fs.createReadStream(inputFile, {encoding: 'utf8'}).pipe(split());
+
+    var featureCollection = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+
+    var start = '{"type": "FeatureCollection", "features": [';
+    fs.appendFileSync(outputFile, start, {encoding: 'utf8'});
+    var comma = "";
+    var line = 0;
+    inputStream.on('data', function (chunk) {
+        line = line + 1;
+        process.stderr.cursorTo(0);
+        process.stderr.write('Processing line: ' + String(line));
+        if (chunk) {
+            var features = JSON.parse(chunk).features;
+            features.forEach(function (feature) {
+                fs.appendFileSync(outputFile, comma + JSON.stringify(feature), {encoding: 'utf8'});
+                if (!comma) {
+                    comma = ',';
+                }
+            });
+        }
+    });
+
+    inputStream.on('end', function () {
+        var end = ']}';
+        fs.appendFileSync(outputFile, end, {encoding: 'utf8'});
+        console.log('\ndone');
+    });
 }
