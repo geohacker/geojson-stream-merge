d4eeade44082595e5b9400c60e3552c6e75ba35a
amishas157 <amishas157@gmail.com>
2016-07-28 00:14:17 +0530

    Add teardown

--- test/test.js
+++ test/test.js
@@ -12,25 +12,31 @@ tape('error when --input argument is not present', function(assert) {
 });
 
 tape('Check output file name when output file name argument is empty', function(assert) {
-  geojsonStreamMerge("test.geojson", null,function(err,data) {
-    assert.equals(data, "test-merged.geojson", "ok, valid file name");
+  geojsonStreamMerge(__dirname + "/test.geojson", null,function(err,data) {
+    assert.equals(data, __dirname + "/test-merged.geojson", "ok, valid file name");
     assert.end();
   })
 
 });
 
 tape('check if the output is as per expected', function(assert) {
-  geojsonStreamMerge("test.geojson", "output.geojson",function(err,data) {
-    var output = fs.readFileSync('output.geojson', 'utf8');
-    var testOutput = fs.readFileSync('testOutput.geojson', 'utf8');
+  geojsonStreamMerge(__dirname + "/test.geojson", __dirname + "/output.geojson",function(err,data) {
+    var output = fs.readFileSync(__dirname + '/output.geojson', 'utf8');
+    var testOutput = fs.readFileSync(__dirname + '/testOutput.geojson', 'utf8');
     assert.equals(output, testOutput, "ok, valid output");
     assert.end();
   })
 
 });
 
-tape('check if the output is vaildated', function(assert) {
-    var output = fs.readFileSync('output.geojson', 'utf8');
-    geojsonAssert(output);
-    assert.end();
+tape('is geojson valid', function(assert) {
+  var output = fs.readFileSync(__dirname + '/output.geojson', 'utf8');
+  assert.equals(geojsonAssert(output), undefined, "ok, valid geojson");
+  assert.end();
+});
+
+tape('teardown', function(assert) {
+  fs.unlinkSync(__dirname + "/output.geojson");
+  fs.unlinkSync(__dirname + "/test-merged.geojson");
+  assert.end();
 });
