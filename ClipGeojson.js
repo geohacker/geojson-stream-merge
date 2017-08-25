a5a714c4995f30bdc847e598322ba0e0f550a75e
Fa7C0n <gokul.ganesan@mapbox.com>
2017-08-25 17:52:08 +0530

    updated the json objects

--- ClipGeojson.js
+++ ClipGeojson.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var turf = require('turf');
 var minimist = require('minimist');
+var split = require('split');
 
 function ClipGeojson(bbox, clip, outFile, callback) {
     if (!bbox) {
@@ -20,21 +21,24 @@ function ClipGeojson(bbox, clip, outFile, callback) {
         
         return callback(new Error('--clip argument needed'));
     }
+      
     var start = '{ "type": "FeatureCollection", "features": [';    
     fs.appendFileSync(outFile, start, {encoding: 'utf8'});
     var comma = '';
     var line = 0;
     fs.createReadStream(clip, {encoding: 'utf8'})
+        .pipe(split())
         .on('data', function(point) {
-            console.log(point);
-            if (!point) return;
+            var json = JSON.parse(point);
+            var bbox = JSON.parse(fs.readFileSync(bbox, 'utf-8'));
+            console.log(json);
+            if (!json) return;
             
             line = line + 1;
             process.stderr.cursorTo(0);
             process.stderr.write('Processing line: ' + String(line));
 
-            if (turf.inside(point, bbox)) {
-                var json = JSON.parse(point);
+            if (turf.inside(json.geometry, bbox)) {                
                 fs.appendFileSync(outFile, comma + JSON.stringify(json), {encoding: 'utf8'});
                     if (!comma) {
                         comma = ',';
@@ -44,6 +48,7 @@ function ClipGeojson(bbox, clip, outFile, callback) {
         .on('end', function () {
                 var end = "]}";
                 fs.appendFileSync(outFile, end, {encoding: 'utf8'});
+                console.log(outFile);
                 console.log('\nMerged features in %s', outFile);
                 if (callback) {
                     callback(null, outFile);
@@ -53,6 +58,6 @@ function ClipGeojson(bbox, clip, outFile, callback) {
 
 var argv = require('minimist')(process.argv.slice(2));
 
-console.log(argv)
+// console.log(argv)
 
 ClipGeojson(argv.bbox, argv.clip, argv.outFile, function(err, done) { console.log('callback called', err, done); })
