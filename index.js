af9b7f7b3d5b34906a90751d67b66d45a5d24bbc
arunasank <aruna@mapbox.com>
2017-09-19 14:18:55 +0530

    readline

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var split = require('split');
+const readline = require('readline');
 
 function geojsonStreamMerge(inputFile, outputFile, callback) {
     if (!inputFile) {
@@ -25,7 +26,7 @@ function geojsonStreamMerge(inputFile, outputFile, callback) {
     var line = 0;
     inputStream.on('data', function (chunk) {
         line = line + 1;
-        process.stderr.cursorTo(0);
+        readline.cursorTo(process.stderr, 0);
         process.stderr.write('Processing line: ' + String(line));
         if (chunk) {
             var json = JSON.parse(chunk);
